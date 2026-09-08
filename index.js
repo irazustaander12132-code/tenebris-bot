6522b51c2bbc712c08d4b617b43dc2500f7eb529
irazustaander12132-code <irazustaander12132@gmail.com>
2026-09-08 18:31:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ client.on('messageCreate', async (message) => {
    try {
         const chatCompletion = await groq.chat.completions.create({
             messages: messagesToSend,
-            model: "llama-3.3-70b-versatile",
+            model: "llama-3.1-8b-instant",
             temperature: 0.5,
         });
 
